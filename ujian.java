910028b34257846b3199e7de25c2e41274047e2a
aftacreative <aftaam6@gmail.com>
2024-11-18 09:15:22 +0700

    Create ujian.java

---
+++ ujian.java
@@ -0,0 +1,32 @@
+public class Ujian {
+    private int nilai;
+    private String nama;
+    private String nim;
+
+    // Getter dan setter untuk nama
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    // Getter dan setter untuk NIM
+    public void setNIM(String nim) {
+        this.nim = nim;
+    }
+
+    public String getNIM() {
+        return nim;
+    }
+
+    // Getter dan setter untuk nilai
+    public void setNilai(int nilai) {
+        this.nilai = nilai;
+    }
+
+    public int getNilai() {
+        return nilai;
+    }
+}
